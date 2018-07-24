2d583819a80a02c664c7cba0f3413db070a4c534
at-thongtran <thongtran@asiantech.com>
2018-07-24 10:26:06 +0700

    layout admin

--- resources/lang/en/admin/admin.php
+++ resources/lang/en/admin/admin.php
@@ -1,23 +1,28 @@
 <?php
 
-// resources/lang/en/messages.php
+    /*
+    |--------------------------------------------------------------------------
+    | Admin layout localization
+    |--------------------------------------------------------------------------
+    |
+    */
 
-return [
-	'title' => 'Admin',
-	'dashboard' => 'Dashboard',
-	'Users' => 'Users',
-	'List User' => 'List User',
-	'Add User' => 'Add User',
-	'Hotels' => 'Hotels',
-	'List Hotels' => 'List Hotels',
-	'Add Hotel' => 'Add Hotel',
-	'Print invoice' => 'Print invoice',
-	'Home' => 'Home',
-	'Contract' => 'Contract',
-	'Description' => 'Description',
-	'welcome' => 'Hello',
-	'Manage Accounts' => 'Manage Accounts',
-	'Change Password' => 'Change Password',
-	'Check Inbox' => 'Check Inbox',
-	'Sign Out' => 'Sign Out',
-];
+    return [
+    'title' => 'Admin',
+    'dashboard' => 'Dashboard',
+    'Users' => 'Users',
+    'List User' => 'List User',
+    'Add User' => 'Add User',
+    'Hotels' => 'Hotels',
+    'List Hotels' => 'List Hotels',
+    'Add Hotel' => 'Add Hotel',
+    'Print invoice' => 'Print invoice',
+    'Home' => 'Home',
+    'Contract' => 'Contract',
+    'Description' => 'Description',
+    'welcome' => 'Hello',
+    'Manage Accounts' => 'Manage Accounts',
+    'Change Password' => 'Change Password',
+    'Check Inbox' => 'Check Inbox',
+    'Sign Out' => 'Sign Out',
+    ];
