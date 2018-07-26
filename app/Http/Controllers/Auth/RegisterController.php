30c83bcda2c095ca69a124ad0570a4a7a8b09381
at-thongtran <thongtran@asiantech.com>
2018-07-26 10:00:23 +0700

    fix email register

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -61,7 +61,7 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
+            'email' => 'required|string|email|max:255|unique:users, email, admin.@gmail.com',
             'phone' => 'required|regex:/(01)[0-9]{9}/',
             'password' => 'required|string|min:6|confirmed',
         ]);
