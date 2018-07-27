6bbd6b521d562dbac1e3fbd777c1be30c20e07ce
Tri Nguyen V <40163018+at-tringuyen3@users.noreply.github.com>
2018-07-27 11:28:54 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Model implements Authenticatable
-{   
+{
     /**
      * The attributes that are mass assignable.
      *
