9d40b8617887750ba0e7b0019bd1d85940cc2a92
at-thongtran <thongtran@asiantech.com>
2018-07-27 14:54:01 +0700

    fix issue

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::group(['prefix' => 'admin','middleware' => 'AdminMiddleware'], function()
         return view('admin.layout.index');	
 	  });
 
-    Route::resource('hotel', 'HotelController');
+    Route::resource('hotels', 'HotelController');
 });
 
 Auth::routes();
