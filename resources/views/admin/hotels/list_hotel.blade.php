e3ace6105adb89e8729bdeade0ea8226572296d7
Thong Tran <40163209+at-thongtran@users.noreply.github.com>
2018-07-31 01:07:10 +0700

    Update list_hotel.blade.php

--- resources/views/admin/hotels/list_hotel.blade.php
+++ resources/views/admin/hotels/list_hotel.blade.php
@@ -25,39 +25,7 @@
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    @foreach ($hotel as $item)
-                                    <tr class="odd gradeX" align="center">
-                                        <td>{{$item->id}}</td>
-                                        <td>
-                                            <img src="upload/hotel/{{$item->image}}" alt="no image" class="rounded">
-                                            <p>{{$item->name}}</p>
-                                        </td>
-                                        <td>{{$item->user->username}}</td>
-                                        <td>{{$item->address}}</td>
-                                        <td>{{$item->city->city}}, {{$item->city->country}}</td>
-                                        <td>{{$item->number_star}} {{ __('admin/hotel.hotel_list.hotel_star') }}</td>
-                                        <td>
-                                            @if ($item->status == 1)
-                                                {{ __('admin/hotel.hotel_list.hotel_status_enable') }}
-                                            @else
-                                                {{ __('admin/hotel.hotel_list.hotel_status_disable') }}
-                                            @endif
-                                        </td>
-                                        <td>{{$item->descript}}</td>
-                                        <td>
-                                            <a href="{{ route('hotels.edit', $item->id) }}" class="btn btn-sm btn-outline-success"><i class="fas fa-edit">{{ __('admin/hotel.hotel_list.hotel_edit') }}</i></a>
-                                        </td>
-                                        <td class="center">
-                                            <form action="{{route("hotels.destroy", $item->id)}}" method="post">
-                                                @method('DELETE')
-                                                @csrf
-                                                <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('{{ __('admin/hotel.hotel_list.hotel_confirm') }} {{$item->name}}')">
-                                                    <i class="fas fa-trash-alt">{{ __('admin/hotel.hotel_list.hotel_delete') }}</i>
-                                                </button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                    @endforeach
+                                    
                                 </tbody>
                             </table>
                         </div>
