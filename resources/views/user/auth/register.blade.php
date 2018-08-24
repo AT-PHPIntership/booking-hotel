6e59bbc439f382b196c2a0a61f35e8de1485574e
Thong Tran <40163209+at-thongtran@users.noreply.github.com>
2018-08-24 08:56:41 +0700

    Update register.blade.php

--- resources/views/user/auth/register.blade.php
+++ resources/views/user/auth/register.blade.php
@@ -13,7 +13,7 @@
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('user/layout.app.name') }}</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
+                                <input id="name" type="text" class="form-control" name="name" value="" required autofocus>
 
                                     <span class="invalid-feedback" role="alert">
                                         <strong></strong>
@@ -25,7 +25,7 @@
                             <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('user/layout.app.email_address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+                                <input id="email" type="email" class="form-control" name="email" value="" required>
 
                                     <span class="invalid-feedback" role="alert">
                                         <strong></strong>
@@ -37,7 +37,7 @@
                             <label for="address" class="col-md-4 col-form-label text-md-right">{{ __('user/layout.app.address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="address" type="text" class="form-control{{ $errors->has('address') ? ' is-invalid' : '' }}" name="address" value="{{ old('address') }}" required autofocus>
+                                <input id="address" type="text" class="form-control" name="address" value="" required autofocus>
 
                                     <span class="invalid-feedback" role="alert">
                                         <strong></strong>
@@ -50,7 +50,7 @@
                             <label for="phone" class="col-md-4 col-form-label text-md-right">{{ __('user/layout.app.phone') }}</label>
 
                             <div class="col-md-6">
-                                <input id="phone" type="text" class="form-control{{ $errors->has('phone') ? ' is-invalid' : '' }}" name="phone" value="{{ old('phone') }}" required autofocus>
+                                <input id="phone" type="text" class="form-control" name="phone" value="" required autofocus>
 
                                 <span class="invalid-feedback" role="alert">
                                     <strong></strong>
@@ -62,7 +62,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('user/layout.app.password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                                <input id="password" type="password" class="form-control" name="password" required>
 
                                 <span class="invalid-feedback" role="alert">
                                     <strong></strong>
