2901c571d89596d6621ac64511ec1480f07418d7
at-thongtran <thongtran@asiantech.com>
2018-08-24 13:45:44 +0700

    fix issue

---
+++ config/user_define.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Define value for front end
+    |--------------------------------------------------------------------------
+    */
+    // Status for Booked Room view
+    'side_bar' => [
+    	'one_person' => '1',
+        'two_people' => '2',
+        'three_people' => '3',
+        'four_people' => '4',
+        'five_people_plus' => '5+',
+        'price_from_1' => '50000',
+        'price_from_2' => '200000',
+        'price_from_3' => '300000',
+        'price_from_4' => '400000',
+        'price_from_5' => '1000000',
+        'price_to_1' => '100000',
+        'price_to_2' => '300000',
+        'price_to_3' => '400000',
+        'price_to_4' => '500000',
+        'price_to_5' => '99000000',
+    ],
+];
